76e7fd173c72643944ce40e29f5821bfaad8ce12
Jonathan Choi <jonchoi11@gmail.com>
2021-09-01 21:10:15 -0600

    build and add state to form component with associated stories

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import InterviewerListItem from 'components/InterviewerListItem';
-import 'components/InterviewerListItem.scss';
+import 'components/InterviewerList.scss';
 
 export default function InterviewerList(props) {
   const interviewers = props.interviewers.map(interviewer => {
@@ -9,8 +9,8 @@ export default function InterviewerList(props) {
       key={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      selected={interviewer.id === props.interviewer}
-      setInterviewer={event => props.setInterviewer(interviewer.id)}
+      selected={interviewer.id === props.value}
+      setInterviewer={() => props.onChange(interviewer.id)}
       />
     )
   });
