a5fd4264c10fdfc1a7b91caaa0ac598e1b43bccf
Jonathan Choi <jonchoi11@gmail.com>
2021-09-03 16:22:39 -0600

    build custom hooks with associated tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,23 @@ import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  
-  return {mode};
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace=false) {
+    if (replace) {
+      history.pop();
+      setHistory([...history, newMode])
+    } else {
+      setHistory([...history, newMode])
+    }
+    setMode(newMode);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length -1])
+    }
+  }
+  return {mode, transition, back};
 }
